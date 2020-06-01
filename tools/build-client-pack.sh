627bad0049ed421afe0a2a48254cdc04c0150a76
NabilZaman <nabilzaman1@gmail.com>
2020-05-31 21:05:45 -0700

    Fix build script to set up directory structure.

--- tools/build-client-pack.sh
+++ tools/build-client-pack.sh
@@ -7,8 +7,15 @@ project_root=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
 # Subshell to go there and build the archive
 (
     cd $project_root/..;
-    mkdir -p build/;
-    rm build/agrarian_skies_2.zip;
-    zip -r9 build/agrarian_skies_2.zip config/ maps/ mods/ resources/ scripts/ manifest.json
+    rm -rf build/
+    mkdir -p build/temp/agrarian_skies_2/overrides;
+    cp manifest.json build/temp/agrarian_skies_2/;
+    cp -r config maps mods resources scripts build/temp/agrarian_skies_2/overrides/;
+    # subshell aganin because zip builds up the relative path in the archive
+    (
+        cd build/temp;
+        zip -r9 ../agrarian_skies_2.zip agrarian_skies_2;
+    )
+    rm -rf build/temp
 )
 
